774243f95ad0751c693d6c542429dd9ff2efb10b
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-25 04:16:01 +0000

    Added Fontawsome

--- my-app/src/components/Info.js
+++ my-app/src/components/Info.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import icon from '../images/logo192.png';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 export default function Info() {
     return(
         <div className='info-section'>
@@ -8,7 +10,10 @@ export default function Info() {
             <h2 className='name'>Laura Smith</h2>
             <p className='occupation'>Frontend Developer</p>
             <small className='email-add'>laurasmith.website</small>
-            <button className='email-btn'> Email </button>
+            <div className='email-btn'>
+            <a href="mailto:muzhda.noorzad4@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} /> Email </a>
+            </div>
         </div>
 
     );
