2965f4a847c61dd5e557519d64bd18ef46f4d8ce
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-25 05:18:14 +0000

    Added footer styling and structure

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+
+
 
 export default function Footer() {
     return(
-        <div className='footer-section'>
-            <h1>Footer me</h1>
+        <div className='footer'>
+            <FontAwesomeIcon icon={faInstagramSquare} className='icons' />
+            <FontAwesomeIcon icon={faFacebookSquare} className='icons' />
+            <FontAwesomeIcon icon={faLinkedin} className='icons' />
+            <FontAwesomeIcon icon={faGithubSquare} className='icons' />    
         </div>
 
     );
