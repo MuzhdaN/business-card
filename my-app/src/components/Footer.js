2d4edfda923d647258731f6c0cd34ab66f3a1b06
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-03-05 17:27:33 +0600

    Added link for icons in footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
-import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
-
 
 
 export default function Footer() {
     return(
         <div className='footer'>
-            <FontAwesomeIcon icon={faInstagramSquare} className='icons' />
-            <FontAwesomeIcon icon={faFacebookSquare} className='icons' />
-            <FontAwesomeIcon icon={faLinkedin} className='icons' />
-            <FontAwesomeIcon icon={faGithubSquare} className='icons' />    
-        </div>
+            <a href='https://www.linkedin.com/in/muzhda-noorzad/'>
+                <FontAwesomeIcon icon={faLinkedin} className='icons' /> 
+            </a>
 
+            <a href='https://github.com/MuzhdaN'>
+                <FontAwesomeIcon icon={faGithubSquare} className='icons' /> 
+            </a>        
+        </div>
     );
 }
